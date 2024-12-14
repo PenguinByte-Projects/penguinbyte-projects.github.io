d52f1a8a72dbc8940d4c1732e47497c9b3288976
PenguinByte <avalanche77@zohomail.com>
2024-12-14 14:09:37 -0500

    Addition of PenguinByte Guard Legacy to site
    Adding PenguinByte Guard Legacy's webpages to https://penguinbyte-projects.github.io to organize everything under PenguinByte's GitHub page.

---
+++ guard/legacy/pb-about.js
@@ -0,0 +1,11 @@
+function privacypolicy(){
+    location.href = "/archive/penguinbyte-guard/privacypolicy.html"
+}
+
+function sourcecode(){
+    window.location.href = "https://github.com/lexerotk/penguinbyte-guard/"
+}
+
+function termsofservice(){
+    location.href = "/archive/penguinbyte-guard/termsofservice.html"
+}
