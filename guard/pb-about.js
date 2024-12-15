ad7fb00829c605af25b77c05bbe7123ac8f297c3
PenguinByte <avalanche77@zohomail.com>
2024-12-15 12:22:57 -0500

    Fix unnecessary dir [2/2]

---
+++ guard/pb-about.js
@@ -0,0 +1,11 @@
+function privacypolicy(){
+    location.href = "/guard/privacypolicy.html"
+}
+
+function sourcecode(){
+    window.location.href = "https://github.com/lexerotk/penguinbyte-guard/"
+}
+
+function termsofservice(){
+    location.href = "/guard/termsofservice.html"
+}
