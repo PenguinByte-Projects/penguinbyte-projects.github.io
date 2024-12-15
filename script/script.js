9f2c30d861c26339594465d9a2795bfea79e910c
Heinrich-XIAO <74563446+Heinrich-XIAO@users.noreply.github.com>
2024-12-15 17:57:25 -0500

    Fix bug

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-const isMobile = navigator.userAgentData.mobile;
+const isMobile = typeof screen.orientation !== 'undefined';
 console.log("Mobile User: " + isMobile)
 if (isMobile==true) {
 	document.getElementsByTagName("head")[0].innerHTML += '<link rel="stylesheet" href="/style/mobile.css">'
